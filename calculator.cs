59210d8e2de627194a38769d7dc473fc1b598b79
wakhid <addnan.nurwakhid@students.amikom.ac.id>
2020-03-10 12:13:53 +0700

    menambahkan fungsi lain

--- calculator.cs
+++ calculator.cs
@@ -2,11 +2,21 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		console.WriteLine("hasil penambahan: {0} + {1} = {2}", a, b, penambahan(a, b));
+		console.WriteLine("hasil pengurangan: {0} - {1} = {2}", a, b, pengurangan(a, b));
 		
+		console.WriteLine("\nTekan sembarang key untuk keluar");
+		console.readkey	
 	}
 	static int penambahan(int a, int b)
 	{
 		return a + b;
+	}
+	static int pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
 
